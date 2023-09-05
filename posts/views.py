4a2a22e9a1ca963acdf973296a493e438af649a0
christopherece <christopheranchetaece@gmail.com>
2023-09-05 22:53:31 +1200

    Fix font and padding

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 # Create your views here.
-def index(request):
-    return render(request, 'posts/index.html')
 
-def post(request):
-    return render(request, 'posts/post.html')
+
+def post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'posts/post.html', context)
 
 def search(request):
     return render(request, 'posts/search.html')
