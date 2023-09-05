017ef0eacdbc35af76ed1bf8529dd5cd6f1924fb
christopherece <christopheranchetaece@gmail.com>
2023-09-05 21:49:47 +1200

    Added Pagination

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from posts.models import Category, Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_at').filter(is_published=True)
+
+    paginator = Paginator(posts, 6)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
     context = {
-        'posts': posts
+        'posts': paged_posts
     }
     return render(request, 'pages/index.html', context)
 
