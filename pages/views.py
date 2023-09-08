410c9b4e0678ee26b0df869a47d0be259b4738cf
christopherece <christopheranchetaece@gmail.com>
2023-09-08 16:58:02 +1200

    Fixed Date and remove time

--- pages/views.py
+++ pages/views.py
@@ -6,10 +6,10 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def index(request):
     posts = Post.objects.order_by('-created_at').filter(is_published=True)
-
     paginator = Paginator(posts, 6)
     page = request.GET.get('page')
     paged_posts = paginator.get_page(page)
+    
     context = {
         'posts': paged_posts
     }
